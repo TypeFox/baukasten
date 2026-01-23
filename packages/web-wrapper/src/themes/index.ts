d259a2722c58c38eeceda5837c4dbecb4198c568
Soulaymen Chouri <soulaymen.chouri@typefox.io>
2026-01-23 18:52:04 +0100

    Reduce themes to light/dark

--- packages/web-wrapper/src/themes/index.ts
+++ packages/web-wrapper/src/themes/index.ts
@@ -9,38 +9,20 @@
  */
 
 export type { VSCodeTheme } from "./types";
-export { lightPlus } from "./light-plus";
-export { lightQuiet } from "./light-quiet";
-export { lightSolarized } from "./light-solarized";
 export { lightModern } from "./light-modern";
-export { darkPlus } from "./dark-plus";
 export { darkModern } from "./dark-modern";
-export { darkSolarized } from "./dark-solarized";
-export { darkMonokai } from "./dark-monokai";
 
 import { VSCodeTheme } from "./types";
 
 import { lightModern } from "./light-modern";
-import { lightPlus } from "./light-plus";
-import { lightQuiet } from "./light-quiet";
-import { lightSolarized } from "./light-solarized";
-import { darkPlus } from "./dark-plus";
 import { darkModern } from "./dark-modern";
-import { darkSolarized } from "./dark-solarized";
-import { darkMonokai } from "./dark-monokai";
 
 /**
  * All available themes
  */
 export const themes: VSCodeTheme[] = [
-  lightPlus,
-  lightQuiet,
-  lightSolarized,
   lightModern,
-  darkPlus,
   darkModern,
-  darkSolarized,
-  darkMonokai,
 ];
 
 /**
@@ -51,6 +33,6 @@ export const getThemeById = (id: string): VSCodeTheme | undefined => {
 };
 
 /**
- * Default theme (Dark+)
+ * Default theme (Light Modern)
  */
 export const defaultTheme = lightModern;
